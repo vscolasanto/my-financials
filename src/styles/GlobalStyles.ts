22cccdd087c6d54f13b2721d1dfe50a32d912534
Vinicius Colasanto <vscolasanto@gmail.com>
2021-03-13 17:07:37 -0300

    chore: add styled-components

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    width: 100%;
+    height: 100vh;
+    background: #e2e3e4;
+  }
+
+  *, button, input {
+    border: none;
+    outline: none;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
