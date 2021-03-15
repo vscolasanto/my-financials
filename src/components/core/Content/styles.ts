8a34827427b215d521b2810a3732deeb9be4144c
Vinicius Colasanto <vscolasanto@gmail.com>
2021-03-15 10:59:24 -0300

    feat: style scrollbar and fixing aside and header position

--- src/components/core/Content/styles.ts
+++ src/components/core/Content/styles.ts
@@ -5,4 +5,19 @@ export const Container = styled.div`
   background-color: ${(props) => props.theme.colors.primary};
   color: ${(props) => props.theme.colors.white};
   padding: 2.5rem;
+  height: calc(100vh - 60px);
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 1rem;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${(props) => props.theme.colors.secondary};
+    border-radius: 1rem;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: ${(props) => props.theme.colors.tertiary};
+  }
 `
